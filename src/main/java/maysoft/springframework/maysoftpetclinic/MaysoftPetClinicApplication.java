9a5a1cd171ea7640823396159ae803b19ecabdcd
mahmutaydemir <mahmutaydemir@outlook.com>
2020-02-06 12:13:42 +0000

    Initial Project Add

---
+++ src/main/java/maysoft/springframework/maysoftpetclinic/MaysoftPetClinicApplication.java
@@ -0,0 +1,13 @@
+package maysoft.springframework.maysoftpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaysoftPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaysoftPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/maysoft/springframework/maysoftpetclinic/MaysoftPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package maysoft.springframework.maysoftpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaysoftPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
