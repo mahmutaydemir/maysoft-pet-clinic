6c2e421d3aa74d9d000af19fe026fdc3a0b0979c
mahmutaydemir <mahmutaydemir@outlook.com>
2020-02-08 21:27:34 +0000

    refactoring to common crud interface. closes #11.

--- pet-clinic-data/src/main/java/maysoft/springframework/maysoftpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/maysoft/springframework/maysoftpetclinic/services/PetService.java
@@ -2,12 +2,6 @@ package maysoft.springframework.maysoftpetclinic.services;
 
 import maysoft.springframework.maysoftpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet );
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/maysoft/springframework/maysoftpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/maysoft/springframework/maysoftpetclinic/services/VetService.java
@@ -2,12 +2,6 @@ package maysoft.springframework.maysoftpetclinic.services;
 
 import maysoft.springframework.maysoftpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
