a3246d8b2621115634f639b60e275c8f8bb4a0f5
mahmutaydemir <mahmutaydemir@outlook.com>
2020-02-11 20:48:43 +0000

    Adding Owners List and Updated Controller. Closes #16.

--- pet-clinic-web/src/main/java/maysoft/springframework/maysoftpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/maysoft/springframework/maysoftpetclinic/controllers/OwnerController.java
@@ -1,15 +1,25 @@
 package maysoft.springframework.maysoftpetclinic.controllers;
 
+import maysoft.springframework.maysoftpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     //@RequestMapping({"/owners", "/owners/index","/owners/index.html"})    // slas lardan onceki yazilan owners lari yukari alabilirim.
-    @RequestMapping({"","/", "/index","/index.html"})                       // yukardakinden farkli olarak bas tarafa bos bir tirnak ekleniyor ekstra.
-    public String listOwners() {
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    // yukardakinden farkli olarak bas tarafa bos bir tirnak ekleniyor ekstra.
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
