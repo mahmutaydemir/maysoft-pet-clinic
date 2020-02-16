029869e3f167ef0ffd256ef90c7b7245296e7f40
mahmutaydemir <mahmutaydemir@outlook.com>
2020-02-16 15:50:42 +0000

    Added Specialities Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/maysoft/springframework/maysoftpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package maysoft.springframework.maysoftpetclinic.services;
+
+import maysoft.springframework.maysoftpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
