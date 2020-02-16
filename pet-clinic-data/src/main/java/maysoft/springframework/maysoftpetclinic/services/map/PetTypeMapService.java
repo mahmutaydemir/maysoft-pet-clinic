738fe0604d7f4243233feaa5a932e6e0fc20c57f
mahmutaydemir <mahmutaydemir@outlook.com>
2020-02-16 14:51:35 +0000

    Added PetTypes to BootStrap. Closes #32.

--- pet-clinic-data/src/main/java/maysoft/springframework/maysoftpetclinic/services/map/PetTypeMapService.java
+++ pet-clinic-data/src/main/java/maysoft/springframework/maysoftpetclinic/services/map/PetTypeMapService.java
@@ -2,9 +2,11 @@ package maysoft.springframework.maysoftpetclinic.services.map;
 
 import maysoft.springframework.maysoftpetclinic.model.PetType;
 import maysoft.springframework.maysoftpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
     @Override
     public Set<PetType> findAll() {
--- pet-clinic-web/src/main/java/maysoft/springframework/maysoftpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/maysoft/springframework/maysoftpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package maysoft.springframework.maysoftpetclinic.bootstrap;
 
 import maysoft.springframework.maysoftpetclinic.model.Owner;
+import maysoft.springframework.maysoftpetclinic.model.PetType;
 import maysoft.springframework.maysoftpetclinic.model.Vet;
 import maysoft.springframework.maysoftpetclinic.services.OwnerService;
+import maysoft.springframework.maysoftpetclinic.services.PetTypeService;
 import maysoft.springframework.maysoftpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
