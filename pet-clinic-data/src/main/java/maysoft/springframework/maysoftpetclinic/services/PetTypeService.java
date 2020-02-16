e32c57a5d2da066e792792aac3823f1d4e9d62e3
mahmutaydemir <mahmutaydemir@outlook.com>
2020-02-16 14:37:35 +0000

    Added PetTypeMapService. Closes #29.

---
+++ pet-clinic-data/src/main/java/maysoft/springframework/maysoftpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package maysoft.springframework.maysoftpetclinic.services;
+
+import maysoft.springframework.maysoftpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/maysoft/springframework/maysoftpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,33 @@
+package maysoft.springframework.maysoftpetclinic.services.map;
+
+import maysoft.springframework.maysoftpetclinic.model.PetType;
+import maysoft.springframework.maysoftpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
