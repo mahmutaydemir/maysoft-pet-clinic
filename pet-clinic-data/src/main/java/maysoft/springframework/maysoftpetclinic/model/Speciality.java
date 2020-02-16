75a3332a7bf8ca535d987f5870a027d04b9b3649
mahmutaydemir <mahmutaydemir@outlook.com>
2020-02-16 14:20:06 +0000

    Added Specialities, Added to Vet. Closes #27.

---
+++ pet-clinic-data/src/main/java/maysoft/springframework/maysoftpetclinic/model/Speciality.java
@@ -0,0 +1,13 @@
+package maysoft.springframework.maysoftpetclinic.model;
+
+public class Speciality extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
