8c99b659a70fde4311440cca384b44076f95be1e
mahmutaydemir <mahmutaydemir@outlook.com>
2020-03-04 22:40:39 +0000

    Implemented Map Impl for Visit Service. Closes #46.

---
+++ pet-clinic-data/src/main/java/maysoft/springframework/maysoftpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package maysoft.springframework.maysoftpetclinic.services;
+
+import maysoft.springframework.maysoftpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
