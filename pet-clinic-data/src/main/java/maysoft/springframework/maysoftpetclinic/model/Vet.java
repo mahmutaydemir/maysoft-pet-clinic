c2da0a2c9abfd25f9633d3554608746ffe77d957
mahmutaydemir <mahmutaydemir@outlook.com>
2020-03-05 16:13:17 +0000

    Added Project Lombok to PetClinic Project, Closes #51.

--- pet-clinic-data/src/main/java/maysoft/springframework/maysoftpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/maysoft/springframework/maysoftpetclinic/model/Vet.java
@@ -1,23 +1,23 @@
 package maysoft.springframework.maysoftpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
-               inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Specialty> specialities = new HashSet<>();
 
-    public Set<Specialty> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Specialty> specialities) {
-        this.specialities = specialities;
-    }
 }
