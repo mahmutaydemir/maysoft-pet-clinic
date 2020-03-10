ef89db195a4d0a4df77963b2e87d58a2fdf0b1d3
mahmutaydemir <mahmutaydemir@outlook.com>
2020-03-10 18:26:38 +0000

    Upgraded to JUnit5. Closes #4.

--- pet-clinic-web/src/test/java/maysoft/springframework/maysoftpetclinic/MaysoftPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/maysoft/springframework/maysoftpetclinic/MaysoftPetClinicApplicationTests.java
@@ -1,14 +1,15 @@
 package maysoft.springframework.maysoftpetclinic;
 
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class MaysoftPetClinicApplicationTests {
+public class MaysoftPetClinicApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
-
 }
